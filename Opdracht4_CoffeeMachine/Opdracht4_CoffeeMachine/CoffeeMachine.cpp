6cb3b7fbc66a8dfaa87c6634dea8b356e14f70cb
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-11-10 10:42:55 +0100

    Update code to make use of overloading

--- Opdracht4_CoffeeMachine/Opdracht4_CoffeeMachine/CoffeeMachine.cpp
+++ Opdracht4_CoffeeMachine/Opdracht4_CoffeeMachine/CoffeeMachine.cpp
@@ -1,30 +1,23 @@
 #include "CoffeeMachine.h"
 
-void CoffeeMachine::insertCoin(Coin c)
+void CoffeeMachine::insertCoin(OneEuro oneEuro)
 {
-	float coinValue = c.getValue();
-
 	system("cls");
-	std::cout << "You can afford these kinds of coffee with :" << c.getName() << std::endl;
-	std::cout << "Coin Value :" << c.getValue() << std::endl;
+	std::cout << "You Bought a Cappucino!" << std::endl;
 	std::cout << "\n";
+}
 
-	// Ultimately these would be sepperate coffee extending from a 
-	// Coffee super class, with each their own name and cost... 
-	// but lazyness got the best of me. And this is more a proof of concept.
-	if (coinValue >= 0.50f)
-	{
-		std::cout << "Cappucino." << std::endl;
-	}
-	if (coinValue >= 1)
-	{
-		std::cout << "Cafe Latte." << std::endl;
-	}
-	if (coinValue > 2)
-	{
-		std::cout << "Macchiato." << std::endl;
-	}
+void CoffeeMachine::insertCoin(FiftyCents fiftyCents)
+{
+	system("cls");
+	std::cout << "You Bought a Cafe Latte!" << std::endl;
+	std::cout << "\n";
+}
 
+void CoffeeMachine::insertCoin(SchlatCoin schlatCoin)
+{
+	system("cls");
+	std::cout << "You Bought a Macchiato!" << std::endl;
 	std::cout << "\n";
 }
 
@@ -32,7 +25,7 @@ void CoffeeMachine::getCoffeeChoices()
 {
 	system("cls");
 	std::cout << "Available Coffee:" << std::endl;
-	std::cout << "Cappucino." << std::endl;
-	std::cout << "Cafe Latte." << std::endl;
-	std::cout << "Macchiato." << std::endl;
+	std::cout << "Cappucino: 1 Euro" << std::endl;
+	std::cout << "Cafe Latte: 50 cents" << std::endl;
+	std::cout << "Macchiato: 1 Schlat Coin" << std::endl;
 }
--- Opdracht4_CoffeeMachine/Opdracht4_CoffeeMachine/CoffeeMachine.h
+++ Opdracht4_CoffeeMachine/Opdracht4_CoffeeMachine/CoffeeMachine.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <vector>
 #include"Coin.h"
+#include"OneEuro.h"
+#include"FiftyCents.h"
+#include"SchlatCoin.h"
 
 
 class CoffeeMachine
 {
 public:
-	void insertCoin(Coin c);
+	void insertCoin(OneEuro oneEuro);
+	void insertCoin(FiftyCents fiftyCents);
+	void insertCoin(SchlatCoin schlatCoin);
 	void getCoffeeChoices();
 
 private:
